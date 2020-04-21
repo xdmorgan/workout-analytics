831e5be7b3622f5712c468d259709d6614371111
Dan Morgan <hello@danny.codes>
2020-04-20 21:22:46 -0400

    Start to customize design foundations

---
+++ src/button.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import cx from "classnames";
+import Link from "next/link";
+import styles from "./button.module.scss";
+
+export function Button({
+  fill = false,
+  as = undefined,
+  href = undefined,
+  to = undefined,
+  className = undefined,
+  ...props
+}) {
+  const Element = as || (href || to ? Link : "button");
+  return (
+    <Element
+      {...props}
+      to={to}
+      href={href}
+      {...(Element === "button" ? { type: "button" } : {})}
+      className={cx(styles.button, { [styles.fill]: !!fill }, className)}
+    />
+  );
+}
