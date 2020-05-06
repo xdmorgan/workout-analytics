dc3c8807c9d4cc98f1476fa695c9bab5099fa2b7
Dan Morgan <hello@danny.codes>
2020-05-05 21:17:39 -0400

    Update homepage (#19)

--- src/components/file-loader.jsx
+++ src/components/file-loader.jsx
@@ -2,6 +2,7 @@ import React, { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
 import csv from "csvtojson";
 import cx from "classnames";
+import { Button } from "./button";
 
 async function convertCSVToJSON(str) {
   const json = await csv({ checkType: true }).fromString(str);
@@ -40,26 +41,30 @@ export function FileLoader({
   return (
     <div
       className={cx(
-        "d-flex rc-small w-fill",
-        isDragActive ? "bg-g70" : "bg-n70"
+        "d-flex flx-a-c flx-j-c flx-d-c h-fill rc-normal bg-n80 c-n30",
+        isDragActive ? "bg-g80" : "bg-n80",
+        isDragActive ? "c-g20" : "c-n30"
       )}
       style={{
         borderWidth: 2,
         borderStyle: isDragActive ? "solid" : "dashed",
         borderColor: isDragActive ? "var(--color-g60)" : "var(--color-n60)",
-        maxWidth: 420,
         minHeight: 180,
       }}
     >
       <div
         {...getRootProps({
-          className: "d-flex flx-a-c flx-g-1 flx-j-c px-4x py-3x",
+          className: "d-flex w-fill flx-a-c flx-d-c flx-g-1 flx-j-c p-4x",
         })}
       >
         <input {...getInputProps({ accept })} />
-        <div className="child-my-0 c-n40">
-          <p>Drag and drop or click to browse</p>
-        </div>
+        <Button className="mt-3x" size="large" as="div">
+          Browse
+        </Button>
+        <p className="type-small mt-4x mb-0 align-c" style={{ maxWidth: 360 }}>
+          Drag and drop exported CSV file here to start processing or click to
+          browse.
+        </p>
       </div>
     </div>
   );
