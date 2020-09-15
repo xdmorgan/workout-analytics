981b784d4eac55e7646a97f831877f67f4da8c34
Dan Morgan <hello@danny.codes>
2020-09-14 20:51:49 -0400

    Date filtering, finally

--- src/utils/date-range.js
+++ src/utils/date-range.js
@@ -21,7 +21,8 @@ export function getMinimumDate(dates) {
   return dates.reduce((min, cur) => (cur < min ? cur : min));
 }
 
-export function filterByRange({ dates, from, length, clampRange = true }) {
+export function filterByRange({ dates, from, length, clampRange = false }) {
+  console.log(dates);
   // from an array of 'dates' (probs via object.keys), an 'end' date, and a 'range'
   // e.g. dates = ["2020-01-31", "2020-01-30", "2020-02-28", "2020-03-31"]
   // & end = "2020-09-10" & range = RANGE_TYPES.ninetyDays
