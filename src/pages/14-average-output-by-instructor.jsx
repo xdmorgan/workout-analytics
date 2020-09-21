9fa73e86f8d59743328aab3365c140f204b0faa7
Dan Morgan <hello@danny.codes>
2020-09-21 19:36:11 -0400

    Create new page

---
+++ src/pages/14-average-output-by-instructor.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { ContentHeader } from "../components/content-header";
+import { ContentSection } from "../components/content-section";
+import { Pagination } from "../components/pagination";
+import { TRANSFORMED_KEYS } from "../constants";
+
+export const meta = {
+  route: "/example",
+  title: "Example",
+  sidebar: "Example",
+  component: Page,
+  protected: true,
+  pagination: {
+    previous: null,
+    next: null,
+  },
+};
+
+export function Page({ allWorkoutData, pageMetadata }) {
+  const pageWorkoutData = allWorkoutData[TRANSFORMED_KEYS.TopInstructors];
+  console.log(pageWorkoutData, pageMetadata);
+  return (
+    <>
+      <ContentHeader
+        previousRoute={pageMetadata.pagination.previous}
+        nextRoute={pageMetadata.pagination.next}
+      >
+        {pageMetadata.title}
+      </ContentHeader>
+      <ContentSection>
+        <div className="wysiwyg child-my-0">
+          <h2>Hello</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt
+            reiciendis et, nesciunt quis quia eum vel inventore est debitis ab,
+            molestias ex, dolore repellendus? Beatae eos minus laudantium
+            placeat eligendi?
+          </p>
+        </div>
+      </ContentSection>
+      <Pagination
+        previousRoute={pageMetadata.pagination.previous}
+        nextRoute={pageMetadata.pagination.next}
+      />
+    </>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
+// landing page
 import { meta as welcome } from "./00-welcome";
+// linear app views
 import { meta as totals } from "./10-totals";
 import { meta as activity } from "./11-activity";
 import { meta as instructors } from "./12-instructors";
 import { meta as outputs } from "./13-outputs";
+import { meta as averageOutputByInstructor } from "./14-average-output-by-instructor";
+// non-linear supporting pages
 import { meta as styleguide } from "./100-styleguide";
 import { meta as styleguideButtons } from "./101-styleguide-buttons";
 
@@ -14,6 +18,7 @@ export default {
   [activity.route]: activity,
   [instructors.route]: instructors,
   [outputs.route]: outputs,
+  [averageOutputByInstructor.route]: averageOutputByInstructor,
   // supporting pages
   [styleguide.route]: styleguide,
   [styleguideButtons.route]: styleguideButtons,
