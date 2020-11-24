46a6fc4d0a7462c89edb9bd4605cb6e9130a65a3
Dan Morgan <hello@danny.codes>
2020-11-23 21:02:11 -0500

    Update

--- src/utils/date-format.test.js
+++ src/utils/date-format.test.js
@@ -1,7 +1,7 @@
 import { format } from "./date-format";
 
 describe("format()", () => {
-  test("parses formats date for charts", async () => {
+  test("converts date object to YYYYY-MM-DD", async () => {
     expect(format(new Date("12/31/99"))).toBe("1999-12-31");
     expect(format(new Date("1/1/2020"))).toBe("2020-01-01");
   });
