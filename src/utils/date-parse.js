e67af7dc7cf40d702f5330f34112b30e3e17eb5f
Dan Morgan <hello@danny.codes>
2020-11-23 20:30:43 -0500

    how frustrating

--- src/utils/date-parse.js
+++ src/utils/date-parse.js
@@ -1,10 +1,13 @@
-export const parseCSVDate = (input) => {
+import parse from "date-fns/parse";
+
+export const parseCSVDate = (
+  input,
+  format = "yyyy-MM-dd kk:mm",
+  reference = new Date()
+) => {
+  // plan
+  // parseDate() the date to JS Date in whatever format this is
+  // from js Date, convert to utc string
   const [date] = input.split(/[(,)]/);
-  // const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  // console.log(new Date(date).toLocaleString({ timeZone }));
-  // Just parse time as 8:00pm in whatever your current TZ is
-  // that way, it will be right for people in the same tz and who cares if its
-  // not locoalized when on vacation / moved etc. hopefully they yfix the
-  // date type to not be lossy at some point
-  return new Date(date);
+  return parse(date, format, reference);
 };
