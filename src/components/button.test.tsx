dc49db8da904b64bac184b9ab3435c7ff05f6abb
Dan Morgan <hello@danny.codes>
2020-11-28 17:03:12 -0500

    Ts button w tests

---
+++ src/components/button.test.tsx
@@ -0,0 +1,58 @@
+import { render } from '@testing-library/react';
+import { MemoryRouter as Router } from 'react-router-dom';
+import { Button } from './button';
+
+const LABELS = { children: 'hello' };
+
+test('renders <Button /> with required props', () => {
+  const { getByText } = render(<Button>{LABELS.children}</Button>);
+  expect(getByText(LABELS.children)).toBeInTheDocument();
+});
+
+test('renders as custom element with `as` prop', () => {
+  const element = 'span';
+  const { getByText } = render(<Button as={element}>{LABELS.children}</Button>);
+  expect(getByText(LABELS.children).nodeName).toEqual(element.toUpperCase());
+});
+
+test('renders as Link with `to` prop', () => {
+  const element = 'a';
+  const { getByText } = render(
+    <Router>
+      <Button to="/">{LABELS.children}</Button>
+    </Router>
+  );
+  expect(getByText(LABELS.children).nodeName).toEqual(element.toUpperCase());
+});
+
+test('renders as anchor with `href` prop', () => {
+  const element = 'a';
+  const { getByText } = render(
+    <Router>
+      <Button href="/">{LABELS.children}</Button>
+    </Router>
+  );
+  expect(getByText(LABELS.children).nodeName).toEqual(element.toUpperCase());
+});
+
+describe('<Button.Group />', () => {
+  test('renders with required props', () => {
+    const { getByText } = render(
+      <Button.Group>
+        <Button>1</Button>
+        <Button>{LABELS.children}</Button>
+        <Button>3</Button>
+      </Button.Group>
+    );
+    expect(getByText(LABELS.children)).toBeInTheDocument();
+  });
+  test('renders custom element with `as` prop', () => {
+    const element = 'span';
+    const { getByText } = render(
+      <Button.Group>
+        <Button as={element}>{LABELS.children}</Button>
+      </Button.Group>
+    );
+    expect(getByText(LABELS.children).nodeName).toEqual(element.toUpperCase());
+  });
+});
---
+++ src/components/button.tsx
@@ -0,0 +1,61 @@
+import cx from 'classnames';
+import { Link } from 'react-router-dom';
+import styles from './button.module.scss';
+
+interface ButtonProps {
+  as?: any;
+  href?: string;
+  to?: string;
+  className?: string;
+  appearance?: 'primary' | 'secondary' | 'ghost';
+  size?: 'small' | 'large';
+  theme?: 'light' | 'dark';
+  [props: string]: any;
+}
+
+export function Button({
+  href,
+  to,
+  className,
+  size,
+  as = 'button',
+  appearance = 'primary',
+  theme = 'light',
+  ...props
+}: ButtonProps) {
+  let Element = as;
+  if (to) Element = Link;
+  if (href) Element = 'a';
+  return (
+    <Element
+      {...props}
+      to={to}
+      href={href}
+      {...(Element === 'button' ? { type: 'button' } : {})}
+      className={cx(
+        styles.button,
+        {
+          [styles[`button--${theme}`]]: theme,
+          [styles[`button--${theme}--${appearance}`]]: theme && appearance,
+          [styles[`button--${size}`]]: !!size,
+          'is-active': !!props['aria-pressed'],
+        },
+        className
+      )}
+    />
+  );
+}
+
+interface ButtonGroupProps {
+  as: any;
+  className?: string;
+  [props: string]: any;
+}
+
+function Group({ as: Element = 'div', className, ...props }: ButtonGroupProps) {
+  return (
+    <Element role="group" {...props} className={cx(styles.group, className)} />
+  );
+}
+
+Button.Group = Group;
