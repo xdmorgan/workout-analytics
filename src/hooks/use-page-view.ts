bade9f06ead416acedc487051145e85deb6e7e64
Dan Morgan <hello@danny.codes>
2020-12-03 15:33:54 -0500

    again

--- src/hooks/use-page-view.ts
+++ src/hooks/use-page-view.ts
@@ -1,10 +1,11 @@
 import { useCallback, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { usePageMeta } from './use-page-meta';
 
 const WARN_MISSING_SNIPPET =
   'Warning: Using fallback function because window.gtag is missing';
 const WARN_MISSING_ENV_VAR =
-  'Warning: Using fallback function because REACT_APP_ANALYTICS_ID env var is undefined';
+  'Warning: Using fallback function because REACT_APP_GA_MEASUREMENT_ID env var is undefined';
 
 const logDebug: Gtag.Gtag = (...args: any[]) =>
   console.info('usePageView()', ...args);
@@ -15,9 +16,10 @@ const logConfigFallback: Gtag.Gtag = (...args: any[]) =>
 
 export function usePageView({ debug = false }: { debug?: boolean } = {}) {
   const location = useLocation();
+  const meta = usePageMeta(location.pathname || '/');
   // check the gtag script is defined. Otherwise, use fallback and warn about misconfig
   const hasGtag = !!window?.gtag;
-  const hasUAID = !!process?.env?.REACT_APP_ANALYTICS_ID;
+  const hasUAID = !!process?.env?.REACT_APP_GA_MEASUREMENT_ID;
   // its fine, the fns dont have add'l dependencies
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const fn = useCallback(
@@ -30,6 +32,9 @@ export function usePageView({ debug = false }: { debug?: boolean } = {}) {
     [debug, hasGtag, hasUAID]
   );
   useEffect(() => {
-    fn('config', process.env.REACT_APP_ANALYTICS_ID as string);
-  }, [fn, location]);
+    fn('config', process.env.REACT_APP_GA_MEASUREMENT_ID as string, {
+      page_title: meta.title,
+      page_path: meta.route,
+    });
+  }, [fn, meta.title, meta.route]);
 }
