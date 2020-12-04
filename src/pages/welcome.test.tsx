ea7bf750ac025c77b5c21e773ec51c2260325071
Dan Morgan <hello@danny.codes>
2020-12-04 11:19:07 -0500

    Welcome and demo data flow testing

--- src/pages/welcome.test.tsx
+++ src/pages/welcome.test.tsx
@@ -1,15 +1,52 @@
-import React from 'react';
-import { meta } from './welcome';
+import React, { ReactNode } from 'react';
 import { render } from '@testing-library/react';
+import { MemoryRouter as Router } from 'react-router-dom';
 import { WorkoutDataProvider } from '../contexts/workout-data-provider';
+import { meta } from './welcome';
 
-describe('Welcome (Page)', () => {
-  test('Renders Page with meta data', () => {
+const LABELS = {
+  flowStartHeading: 'Start here',
+  flowCompleteHeading: "You're all set!",
+  seeDemoButton: 'See demo',
+  resetStateButton: 'Start over',
+};
+
+function MockedEnv({ children }: { children: ReactNode }) {
+  return (
+    <Router>
+      <WorkoutDataProvider>{children}</WorkoutDataProvider>
+    </Router>
+  );
+}
+
+describe('Renders without crashing', () => {
+  test('Welcomes new users with "start here" message', () => {
     const { getByText } = render(
-      <WorkoutDataProvider>
+      <MockedEnv>
         <meta.component />
-      </WorkoutDataProvider>
+      </MockedEnv>
     );
-    expect(getByText('start here', { exact: false })).toBeInTheDocument();
+    expect(getByText(LABELS.flowStartHeading)).toBeInTheDocument();
+  });
+});
+
+describe('Demo flow', () => {
+  test('Click through the demo data flow', () => {
+    const { getByText } = render(
+      <MockedEnv>
+        <meta.component />
+      </MockedEnv>
+    );
+    // start with welcome
+    expect(getByText(LABELS.flowStartHeading)).toBeInTheDocument();
+    expect(getByText(LABELS.seeDemoButton)).toBeInTheDocument();
+    getByText(LABELS.seeDemoButton).click();
+    // expect that demo data is accepted and ready to proceed to protected pages
+    expect(getByText(LABELS.flowCompleteHeading)).toBeInTheDocument();
+    expect(getByText(LABELS.resetStateButton)).toBeInTheDocument();
+    // now reset data
+    getByText(LABELS.resetStateButton).click();
+    // and back to square one
+    expect(getByText(LABELS.flowStartHeading)).toBeInTheDocument();
   });
 });
