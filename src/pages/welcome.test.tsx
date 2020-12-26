88f8f6dd48b8b8bcb3abf8392448b1e87308a9d8
Dan Morgan <hello@danny.codes>
2020-12-26 12:54:42 -0500

    Welcome and demo data flow testing (#98)
    * Welcome and demo data flow testing
    
    * Uploader flow click-through
    
    * Add note

--- src/pages/welcome.test.tsx
+++ src/pages/welcome.test.tsx
@@ -1,15 +1,70 @@
 import React from 'react';
-import { meta } from './welcome';
 import { render } from '@testing-library/react';
-import { WorkoutDataProvider } from '../contexts/workout-data-provider';
+import { MockedEnv } from '../test-utils';
+import { meta } from './welcome';
 
-describe('Welcome (Page)', () => {
-  test('Renders Page with meta data', () => {
+const LABELS = {
+  flowStartHeading: 'Start here',
+  flowCompleteHeading: "You're all set!",
+  flowUploaderHeading: 'Add data',
+  uploadDataButton: 'Analyze my workouts',
+  uploadCancelButton: 'Cancel',
+  uploadBrowseFilesButton: 'Select file',
+  seeDemoButton: 'See demo',
+  resetStateButton: 'Start over',
+};
+
+describe('Renders without crashing', () => {
+  test('Welcomes new users with "start here" message', () => {
     const { getByText } = render(
-      <WorkoutDataProvider>
+      <MockedEnv>
         <meta.component />
-      </WorkoutDataProvider>
+      </MockedEnv>
     );
-    expect(getByText('start here', { exact: false })).toBeInTheDocument();
+    expect(getByText(LABELS.flowStartHeading)).toBeInTheDocument();
+  });
+});
+
+describe('Demo flow', () => {
+  test('Click through the demo data flow', () => {
+    const { getByText } = render(
+      <MockedEnv>
+        <meta.component />
+      </MockedEnv>
+    );
+    // start with welcome
+    expect(getByText(LABELS.flowStartHeading)).toBeInTheDocument();
+    expect(getByText(LABELS.seeDemoButton)).toBeInTheDocument();
+    getByText(LABELS.seeDemoButton).click();
+    // expect that demo data is accepted and ready to proceed to protected pages
+    expect(getByText(LABELS.flowCompleteHeading)).toBeInTheDocument();
+    expect(getByText(LABELS.resetStateButton)).toBeInTheDocument();
+    // now reset data
+    getByText(LABELS.resetStateButton).click();
+    // and back to square one
+    expect(getByText(LABELS.flowStartHeading)).toBeInTheDocument();
+  });
+});
+
+describe('Upload flow', () => {
+  // TODO: test  upload (requires a bunch of mocks)
+  // https://github.com/testing-library/react-testing-library/issues/93
+  test('Click through the upload data flow but cancel at the uploader?', () => {
+    const { getByText } = render(
+      <MockedEnv>
+        <meta.component />
+      </MockedEnv>
+    );
+    // start with welcome
+    expect(getByText(LABELS.flowStartHeading)).toBeInTheDocument();
+    expect(getByText(LABELS.uploadDataButton)).toBeInTheDocument();
+    getByText(LABELS.uploadDataButton).click();
+    // click to browse files and fake uploading of data
+    expect(getByText(LABELS.flowUploaderHeading)).toBeInTheDocument();
+    expect(getByText(LABELS.uploadBrowseFilesButton)).toBeInTheDocument();
+    expect(getByText(LABELS.uploadCancelButton)).toBeInTheDocument();
+    getByText(LABELS.uploadCancelButton).click();
+    // and back to square one
+    expect(getByText(LABELS.flowStartHeading)).toBeInTheDocument();
   });
 });
---
+++ src/test-utils.tsx
@@ -0,0 +1,24 @@
+import React, { ReactNode } from 'react';
+import { MemoryRouter as Router } from 'react-router-dom';
+import { WorkoutDataProvider } from './contexts/workout-data-provider';
+import { PageMetadataConfig, PageMetadataProp } from './pages/types';
+
+// strip self-component reference and replace with pagination, if needed
+export const convertMetaConfigToProp = ({
+  component,
+  ...rest
+}: PageMetadataConfig): PageMetadataProp => ({
+  ...rest,
+  pagination: { previous: null, next: null },
+});
+
+/**
+ * Wrap children in MemoryRouter and WorkoutDataProvider
+ */
+export function MockedEnv({ children }: { children: ReactNode }) {
+  return (
+    <Router>
+      <WorkoutDataProvider>{children}</WorkoutDataProvider>
+    </Router>
+  );
+}
